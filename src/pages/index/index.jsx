fb259483051a2ac41c71466e884ec6cacaaf76bd
vicedev <vicedev1001@gmail.com>
2020-09-28 21:20:09 +0800

    init

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.less'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+        <AtButton type='primary'>I need Taro UI</AtButton>
+      </View>
+    )
+  }
+}
